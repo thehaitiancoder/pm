ec29dcdbe3552e366223914ac9896b1d63d945e1
RalphGustave <rdgustave@gmail.com>
2018-07-10 15:37:34 -0700

    adding GA tracking code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +7,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // I was trying to set a background image only on the homepage
-  // showBackground= '';
 
-  // constructor(
-  //   private _router: Router
-  // ){
-  //   // if (this._router.url == '/') { this.showBackground = true}
-  //   // else { this.showBackground = false }
-
-  //   setTimeout(() => {
-  //     if (this._router.url == '/') { this.showBackground = 'show'}
-  //     console.log(this._router.url)
-  //   });
-  // }
+  constructor(
+    private _router: Router
+  ){
+    this._router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      }
+    });
+  }
 }
