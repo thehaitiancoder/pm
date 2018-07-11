c6169822cc9b7ee457250350c0dd08905c4ed95e
RalphGustave <rdgustave@gmail.com>
2018-07-10 22:33:28 -0700

    changing link to bg image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
+declare var ga: Function;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
